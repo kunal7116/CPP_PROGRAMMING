91a8b02f4c7c416da60fddae0f3154be2fdcbace
Kunal <69040510+kunal7116@users.noreply.github.com>
2024-04-10 21:52:50 +0530

    Create ADD_TWO_NUMBER.cpp

---
+++ ADD_TWO_NUMBER.cpp
@@ -0,0 +1,10 @@
+//Write a program to adddition of two numbers. 
+#include <iostream>
+using namespace std;
+int main(){
+	int no1,no2,sum;
+	cout<<"Enter two numbers: "<<endl;
+	cin>>no1>>no2;
+	sum = no1+no2;
+	cout<<"Sum is: "<<sum<<endl;
+}
