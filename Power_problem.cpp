f3f16b15a403bd0f77956b081950a41ab6117483
Kunal <69040510+kunal7116@users.noreply.github.com>
2024-04-11 22:11:34 +0530

    Create Power_problem.cpp

---
+++ Power_problem.cpp
@@ -0,0 +1,12 @@
+//Write a program to find m to the power n. m=3 and n=4 so 3*3*3*3
+#include<iostream>
+using namespace std;
+int main9(){
+	int i,m,n,result=1;
+	cout<<"Enter the Number and power:";
+	cin>>m>>n;
+	for(i=1;i<=n;i++){
+		result = result *m;
+	}
+	cout<<"Answer:"<<result;
+}
