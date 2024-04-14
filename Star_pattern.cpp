2a8f434540351f7a8b358b8fe5a88d0a36bdf1d5
Kunal <69040510+kunal7116@users.noreply.github.com>
2024-04-14 14:50:00 +0530

    Create Star_pattern.cpp

---
+++ Star_pattern.cpp
@@ -0,0 +1,11 @@
+//Write a program to print following pattern.
+#include<iostream>
+using namespace std;
+int main6(){
+	for(int i=0;i<=5;i++){
+		for(int j=0;j<=i;j++){
+			cout<<"* ";
+		}
+		cout<<endl;
+    }
+}
